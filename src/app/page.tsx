ae5ccc7dccf34b4b8249c147df43ae24b42261e6
Nolan Pollack <nolanfpollack@gmail.com>
2024-02-29 00:41:01 -0500

    more items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import linkedin from "../assets/linkedin.png";
 import phone from "../assets/phone.png";
 import mail from "../assets/mail.png";
 import github from "../assets/github.png";
+import {ReactNode, useState} from "react";
 
 function H1({title}: { title: string }) {
     return <h1 className="py-4 lg:py-6 lg:text-5xl text-2xl text-gray-600">{title}</h1>
@@ -14,20 +15,21 @@ function H2({title}: { title: string }) {
 }
 
 function H3({title}: { title: string }) {
-    return <h3 className="text-lg text-gray-600">{title}</h3>
+    return <h3 className="text-xl text-gray-600 pb-1">{title}</h3>
 }
 
 function H4({title}: { title: string }) {
-    return <h4 className="text-gray-600 text-sm">{title}</h4>
+    return <h4 className="text-gray-600 text-lg">{title}</h4>
 }
 
-function P({title}: { title: string }) {
-    return <p className="text-gray-600">{title}</p>
+function H5({title}: { title: string }) {
+    return <h5 className="text-gray-600 text-sm">{title}</h5>
 }
 
-function WhiteBox(props: { children: React.ReactNode }) {
+function WhiteBox(props: { children: ReactNode }) {
     // return <div className="relative bg-gray-200 px-6 py-3 shadow-xl shadow-gray-500/20 hover:ring-8 ring-gray-500 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
-    return <div className="justify-center items-center bg-gray-100 shadow-xl rounded-lg max-content px-5 m-2 py-4">
+    return <div
+        className="transition justify-center items-center bg-gray-100 shadow-xl rounded-lg max-content px-5 m-2 py-4 hover:scale-105">
         {props.children}
     </div>
 }
@@ -44,9 +46,9 @@ function ContactSection() {
 
 function ContactItem({title, link, image}: { title: string, link: string, image: any }) {
     return <a href={link} target="_blank" rel="noreferrer">
-        <div className="flex space-x-2">
+        <div className="flex space-x-2 hover:bg-gray-200 px-1.5 text-nowrap rounded-lg py-0.5 active:bg-gray-300">
             <Image className="object-contain" src={image} width={15} height={15} alt={"Phone Logo"}/>
-            <P title={title}/>
+            <p className="text-gray-600 flex-1">{title}</p>
         </div>
     </a>
 }
@@ -57,8 +59,79 @@ function EducationItem({school, degree, date}: { school: string, degree: string,
             <H3 title={school}/>
             <h4 className="tracking-widest text-gray-900 ml-auto">{date}</h4>
         </div>
-        <H4 title={degree}/>
+        <H5 title={degree}/>
     </div>
+}
+
+function WorkExperience({title, company, date, children}: {
+    title: string,
+    company: string,
+    date: string,
+    children: ReactNode
+}) {
+    const [isHovered, setIsHovered] = useState(false);
+
+    return <div className="py-4 group">
+        <div className="flex flex-row">
+            <H3 title={title}/>
+            <h4 className="tracking-widest text-gray-900 ml-auto">{date}</h4>
+        </div>
+        <H4 title={company}/>
+        <ul className="">
+            {children}
+        </ul>
+    </div>
+}
+
+function ExperienceBullets({children}: { children: ReactNode }) {
+    return <ul className="list-disc text-gray-500 list-outside mx-10 space-y-1.5">
+        {children}
+    </ul>
+
+}
+
+function EducationSection() {
+    return <WhiteBox>
+        <div className="divide-y">
+            <H2 title={"Education"}/>
+            <EducationItem school={"Northeastern University"}
+                           degree={"Master of Science in Computer Science"} date={"September 2024 - May 2026"}/>
+            <EducationItem school={"Northeastern University"}
+                           degree={"Bachelor of Science in Computer Science and Business Administration, Concentration in FinTech"}
+                           date={"September 2020 - May 2024"}/>
+        </div>
+    </WhiteBox>
+}
+
+function ExperienceSection() {
+    return <WhiteBox>
+        <div className="divide-y">
+            <H2 title={"Experience"}/>
+            <WorkExperience title={"Software Engineer Co-op"} company={"Toast"} date={"May 2023 - August 2023"}>
+                <ExperienceBullets>
+                    <li>Made numerous code contributions to the production environment focusing on the
+                        integration of the Sling schedule management platform (Python) with Toast platforms
+                        (Kotlin), ensuring seamless data exchange and system compatibility.
+                    </li>
+                    <li>Enriched partner-facing REST API by introducing a country code field, significantly
+                        facilitating internationalization efforts and enhancing live synchronization between
+                        Toast and Sling.
+                    </li>
+                    <li>Implemented a new table in the established Sling database, allowing for efficient
+                        tracking of made API calls to remove redundancy while maintaining data integrity and
+                        association with other key values within the database.
+                    </li>
+                    <li>
+                        Designed an engaging front-end banner using React framework on the homepage of Toast to
+                        drive engagement to Sling, resulting in dozens of new signups.
+                    </li>
+                    <li>
+                        Utilized PyTest and JUnit to rigorously test code, guaranteeing high-quality software.
+                    </li>
+                </ExperienceBullets>
+            </WorkExperience>
+        </div>
+    </WhiteBox>
 }
 
 export default function Home() {
@@ -68,16 +141,20 @@ export default function Home() {
                 <H1 title={"Nolan Pollack"}/>
                 <ContactSection/>
             </div>
-            <WhiteBox>
-                <div className="divide-y">
-                    <H2 title={"Education"}/>
-                    <EducationItem school={"Northeastern University"}
-                                   degree={"Master of Science in Computer Science"} date={"September 2024 - May 2026"}/>
-                    <EducationItem school={"Northeastern University"}
-                                   degree={"Bachelor of Science in Computer Science and Business Administration, Concentration in FinTech"}
-                                   date={"September 2020 - May 2024"}/>
+            <div className="flex flex-row">
+                {/*Left Side*/}
+                <div>
+                    <EducationSection/>
+                    <ExperienceSection/>
                 </div>
-            </WhiteBox>
+                {/*Right Side*/}
+                <div>
+                    <WhiteBox>
+                        <H2 title={"Skills"}/>
+                    </WhiteBox>
+                </div>
+            </div>
         </main>
     );
 }
+
